4ddef9bd3decb76a0f30ef8878cc8240e2610bca
Sergio García <alu0100892260@ull.edu.es>
2017-05-07 23:15:31 +0100

    Terminadas las pruebas

--- test/test.js
+++ test/test.js
@@ -10,69 +10,164 @@ function removeSpaces(str){
 describe("Asignaciones", function() {
   it("a = 4 + 5;", function() {
 
-    var tree = `[ { kind: \'Assignment\',
-    id: \'a\',
-    assignment: [ { kind: \'+\', left: 4, right: 5 } ] } ]`;  // Falta poner aqui lo que devuelve el árbol exactamente con la prueba
+    var tree = `[
+      {
+        "kind": \"ASSIGN\",
+        "id": \"a\",
+        "assignment": [
+          {
+            "kind": \"+\",
+            "left": 4,
+            "right": 5
+          }
+        ]
+      }
+    ]`;  // Falta poner aqui lo que devuelve el árbol exactamente con la prueba
 
-    var resul = JSON.stringify(PEG.parse('a = 4 + 5;'), null, 2);
 
-    expect(resul).to.equal(tree);
+    var tree2 = removeSpaces(tree);
+    var resul = removeSpaces(JSON.stringify(PEG.parse('a = 4 + 5;'), null, 2));
+
+    expect(resul).to.equal(tree2);
   });
 
   it("r = 16; t = r / 4;", function() {
 
-    var tree = `[ { kind: 'ASSIGN', id: 'r', assignment: 16 },
-  { kind: 'ASSIGN',
-    id: 't',
-    assignment: [ { kind: '/', left: 16, right: 4 } ] } ]`;  // Falta poner aqui lo que devuelve el árbol exactamente con la prueba
+    var tree = `[
+      {
+        "kind": "ASSIGN",
+        "id": "r",
+        "assignment": 16
+      },
 
-    var resul = JSON.stringify(PEG.parse('r = 16; t = r / 4;'), null, 2);
+      {
+        "kind": "ASSIGN",
+        "id": "t",
+        "assignment": [
+          {
+            "kind": "/",
+            "left": 16,
+            "right": 4
+          }
+        ]
+      }
+    ]`;  // Falta poner aqui lo que devuelve el árbol exactamente con la prueba
 
-    expect(resul).to.equal(tree);
+    var resul = removeSpaces(JSON.stringify(PEG.parse('r = 16; t = r / 4;'), null, 2));
+    var tree2 = removeSpaces(tree);
+
+    expect(resul).to.equal(tree2);
   });
 });
 
 describe("Funciones", function(){
   it("funcion = function(){ r = 4; };", function() {
-    var tree = `[ { kind: 'ASSIGN',
-    id: 'funcion',
-    assignment:
-     { kind: 'FUNCTION',
-       sentence: [ { kind: 'ASSIGN', id: 'r', assignment: 4 } ] } } ]`;  // Falta poner aqui lo que devuelve el árbol exactamente con la prueba
+    var tree = `[
+      {
+        "kind": "ASSIGN",
+        "id": "funcion",
+        "assignment":
+          {
+            "kind": "FUNCTION",
+            "sentence": [
+              {
+                "kind": "ASSIGN",
+                "id": "r",
+                "assignment": 4
+              }
+            ]
+          }
+        }
+      ]`;  // Falta poner aqui lo que devuelve el árbol exactamente con la prueba
 
-    var resul = JSON.stringify(PEG.parse('funcion = function(){ r = 4; };'), null, 2);
+    var resul = removeSpaces(JSON.stringify(PEG.parse('funcion = function(){ r = 4; };'), null, 2));
+    var tree2 = removeSpaces(tree);
 
-    expect(resul).to.equal(tree);
+    expect(resul).to.equal(tree2);
   });
 });
 
 describe("Condicionales", function() {
   it("a = 4; if (a == 4)  { a = a + 7; };", function(){
-    var tree = `[ { kind: 'ASSIGN', id: 'a', assignment: 4 },
-  { kind: 'IF',
-    condition: { kind: 'CONDITION', right: 4, comp: '==', left: 4 },
-    sentence:
-     [ { kind: 'ASSIGN',
-         id: 'a',
-         assignment: [ { kind: '+', left: 4, right: 7 } ] } ] } ]`;  // Falta poner aqui lo que devuelve el árbol exactamente con la prueba
+    var tree = `[
+      {
+        "kind": "ASSIGN",
+        "id": "a",
+        "assignment": 4
+      },
 
-    var resul = JSON.stringify(PEG.parse('a = 4; if (a == 4)  { a = a + 7; };'), null, 2);
-    expect(resul).to.equal(tree);
+      {
+        "kind": "IF",
+        "condition":
+          {
+            "kind": "CONDITION",
+            "right": 4,
+            "comp": "==",
+            "left": 4
+          },
+
+          "sentence": [
+            {
+              "kind": "ASSIGN",
+              "id": "a",
+              "assignment": [
+                {
+                  "kind": "+",
+                  "left": 4,
+                  "right": 7
+                }
+              ]
+            }
+          ]
+        }
+      ]`;  // Falta poner aqui lo que devuelve el árbol exactamente con la prueba
+
+    var resul = removeSpaces(JSON.stringify(PEG.parse('a = 4; if (a == 4)  { a = a + 7; };'), null, 2));
+    var tree2 = removeSpaces(tree);
+
+    expect(resul).to.equal(tree2);
   });
 });
 
 describe("While", function() {
   it("i = 1; while(i < 3){ i = i + 1; };", function(){
-    var tree = `[ { kind: 'ASSIGN', id: 'i', assignment: 1 },
-  { kind: 'WHILE',
-    condition: { kind: 'CONDITION', right: 1, comp: '<', left: 3 },
-    sentence:
-     [ { kind: 'ASSIGN',
-         id: 'i',
-         assignment: [ { kind: '+', left: 1, right: 1 } ] } ] } ]`;  // Falta poner aqui lo que devuelve el árbol exactamente con la prueba
+    var tree = `[
+      {
+        "kind": "ASSIGN",
+        "id": "i",
+        "assignment": 1
+      },
 
-    var resul = JSON.stringify(PEG.parse('i = 1; while(i < 3){ i = i + 1; };'), null, 2);
+      {
+        "kind": "WHILE",
+        "condition":
+        {
+          "kind": "CONDITION",
+          "right": 1,
+          "comp": "<",
+          "left": 3
+        },
 
-    expect(resul).to.equal(tree);
+        "sentence": [
+          {
+            "kind": "ASSIGN",
+            "id": "i",
+            "assignment": [
+              {
+                "kind": "+",
+                "left": 1,
+                "right": 1
+              }
+            ]
+          }
+        ]
+      }
+    ]`;  // Falta poner aqui lo que devuelve el árbol exactamente con la prueba
+
+    var resul = removeSpaces(JSON.stringify(PEG.parse('i = 1; while(i < 3){ i = i + 1; };'), null, 2));
+    var tree2 = removeSpaces(tree);
+
+
+    expect(resul).to.equal(tree2);
   });
 });
